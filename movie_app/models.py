ef6aed2da78ee57caa3ac79d215240ff9a702db8
DmitriyBurtsev <dm.burcev@yandex.ru>
2023-04-05 07:59:40 +0300

    Added new column

--- movie_app/models.py
+++ movie_app/models.py
@@ -6,3 +6,8 @@ class Movie(models.Model):
     name = models.CharField(max_length=40) # максимальная длина строки
     rating = models.IntegerField() # можно без параметров
     # колонку id не создаю, автоматически создастся в тот момент, когда ползуюсь моделями
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=1000000)
+
+    def __str__(self):
+        return f'{self.name}-{self.rating}%'
--- movie_proj/settings.py
+++ movie_proj/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie_app',
-    'django-extensions',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
