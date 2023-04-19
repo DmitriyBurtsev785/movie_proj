8de00fc225624553b5b453c6558cccce990f0ab6
DmitriyBurtsev <dm.burcev@yandex.ru>
2023-04-19 08:05:06 +0300

    Added class Director

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Movie, Director
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget', 'director']
+    list_editable = ['rating', 'director', 'budget']
+
+admin.site.register(Movie, MovieAdmin)
+
+admin.site.register(Director)
--- movie_app/models.py
+++ movie_app/models.py
@@ -2,12 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class Movie(models.Model):
     name = models.CharField(max_length=40) # максимальная длина строки
     rating = models.IntegerField() # можно без параметров
     # колонку id не создаю, автоматически создастся в тот момент, когда ползуюсь моделями
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=1000000)
+    slug = models.SlugField(default='', null=False, db_index=True)
+    director = models.ForeignKey(Director, on_delete=models.PROTECT, null=True)
+
 
     def __str__(self):
         return f'{self.name}-{self.rating}%'
